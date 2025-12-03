22593227986e6ac9ba904e31a72be2c1151d7433
alvaromena-lab <alvaromena6735@gmail.com>
2025-12-02 23:20:09 -0800

    Playtest 2 update

---
+++ Assets/Scripts/PlayerScripts/FollowMouse.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+
+    private void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        mousePos.z = Mathf.Abs(Camera.main.transform.position.z - transform.position.z);
+
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        Vector2 direction = (mousePos - transform.position);
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+    }
+}
